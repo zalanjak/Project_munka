550213d400c632f08423de6551f790e1038a7eb7
simonnov007@gmail.com <simonnov007@gmail.com>
2022-02-25 08:32:05 +0100

    Simon.cs-t létrehoztam

---
+++ Simon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LogoKaresz
+{
+    public partial class Form1
+    {
+        void csumi()
+        {
+             
+        }
+    }
+}
