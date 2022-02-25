f55f61472d8e878244f6f86b5d9ab6d5880bfee9
Tunde Lelkes <lelkes.tunde.21f@szlgbp.hu>
2022-02-25 08:34:13 +0100

    void köszön

---
+++ Tünde.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+    partial class Form1
+    {
+        void Köszön()
+        {
+            MessageBox.Show("halihó");
+        }
+
+
+    }
+}
